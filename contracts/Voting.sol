2e0cc557ece7821809bcf1dbd9253af5a6f64e74
Piotr Grzesik <pj.grzesik@gmail.com>
2017-05-23 21:01:45 +0200

    Add methods for voting for candidate and related helpers

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -7,7 +7,7 @@ contract Voting {
         uint[] tokensUsedPerCandidate;
     }
 
-    mapping (bytes32 => uint8) public votesReceived;
+    mapping (bytes32 => uint) public votesReceived;
     mapping (address => voter) public voterInfo;
 
     bytes32[] public candidateList;
@@ -22,6 +22,31 @@ contract Voting {
         tokenPrice = _tokenPrice;
     }
 
+    function totalVotesForCandidate(bytes32 candidate) constant returns (uint) {
+        return votesReceived[candidate];
+    }
+
+    function voteForCandidate(bytes32 candidate, uint votesInTokens) {
+        uint index = indexOfCandidate(candidate);
+        if (index == uint(-1)) {
+            throw;
+        }
+
+        if (voterInfo[msg.sender].tokensUsedPerCandidate.length == 0) {
+            for(uint i; i < candidateList.length; i++) {
+                voterInfo[msg.sender].tokensUsedPerCandidate.push(0);
+            }
+        }
+
+        uint availableTokens = voterInfo[msg.sender].tokensBought - totalTokensUsed(voterInfo[msg.sender].tokensUsedPerCandidate);
+        if (availableTokens < votesInTokens) {
+            throw;
+        }
+
+        votesReceived[candidate] += votesInTokens;
+        voterInfo[msg.sender].tokensUsedPerCandidate[index] += votesInTokens;
+    }
+
     function buy() payable returns (uint) {
         uint tokensToBuy = msg.value / tokenPrice;
         if (tokensToBuy > balanceTokens) {
@@ -33,26 +58,41 @@ contract Voting {
         return tokensToBuy;
     }
 
-    function totalVotesFor(bytes32 candidate) returns (uint8) {
-        if (validCandidate(candidate) == false) {
-            throw;
-        }
-        return votesReceived[candidate];
+    function tokensSold() constant returns (uint) {
+        return totalTokens - balanceTokens;
     }
 
-    function voteForCandidate(bytes32 candidate) {
-        if (validCandidate(candidate) == false) {
-            throw;
-        }
-        votesReceived[candidate] += 1;
+    function voterDetails(address user) constant returns (uint, uint[]) {
+        return (voterInfo[user].tokensBought, voterInfo[user].tokensUsedPerCandidate);
     }
 
-    function validCandidate(bytes32 candidate) returns (bool) {
-        for (uint i = 0; i < candidateList.length; i++) {
-            if (candidateList[i] == candidate) {
-                return true;
+    function transferTo(address account) {
+        if (!account.call.value(this.balance)()) {
+            throw;
+        }
+    }
+
+    function allCandidates() constant returns (bytes32[]) {
+        return candidateList;
+    }
+
+
+
+    function indexOfCandidate(bytes32 candidate) constant returns (uint) {
+        for (uint i; i < candidateList.length; i++) {
+            if (candidateList[i] == candidate){
+                return i;
             }
         }
-        return false;
+        return uint(-1);
     }
+
+    function totalTokensUsed(uint[] _tokensUsedPerCandidate) private constant returns (uint) {
+        uint total = 0;
+        for (uint i; i < _tokensUsedPerCandidate.length; i++) {
+            total += _tokensUsedPerCandidate[i];
+        }
+        return total;
+    }
+
 }
